687125e8fff18df12572721d6031b0a30d8439fd
= <=>
2020-11-08 10:53:50 +0700

    fix code

--- DieRoll.java
+++ DieRoll.java
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 public class DieRoll {
-    private int ndice
+    private int ndice;
     private int nsides;
     private int bonus;
 //    private static Random rnd;
